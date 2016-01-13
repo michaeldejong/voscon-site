87b0fe4c931527b8a9a2169d07450b1bd4973610
Michael de Jong <michaelj@minicom.nl>
2016-01-13 21:56:45 +0100

    Added kadekraan to the projects page.

--- voscon.nl/projecten/index.php
+++ voscon.nl/projecten/index.php
@@ -12,62 +12,71 @@
                         </div>
                         <div class="content projects">
                             <div class="project-bar">
-                                <a href="/projecten/m-en-e-huizen/">
+                                <a href="/projecten/kadekraan/">
+                                    <div class="project-item" style="background-image: url(/projecten/kadekraan.jpg);">
+                                        <div class="project-item-overlay">
+                                            <div class="project-item-title">Kadekraan verhogen</div>
+                                        </div>
+                                    </div>
+                                </a>
+                                <a href="/projecten/m-en-e-huizen/" class="pull-right">
                                     <div class="project-item" style="background-image: url(/projecten/m-en-e-huizen.jpg);">
                                         <div class="project-item-overlay">
                                             <div class="project-item-title">E- en M-huizen</div>
                                         </div>
                                     </div>
                                 </a>
-                                <a href="/projecten/transportsystemen/" class="pull-right">
+                            </div>
+                            <div class="project-bar">
+                                <a href="/projecten/transportsystemen/">
                                     <div class="project-item" style="background-image: url(/projecten/transportsystemen.jpg);">
                                         <div class="project-item-overlay">
                                             <div class="project-item-title">Transportsystemen</div>
                                         </div>
                                     </div>
                                 </a>
-                            </div>
-                            <div class="project-bar">
-                                <a href="/projecten/bekleding-wagonbelader/">
+                                <a href="/projecten/bekleding-wagonbelader/" class="pull-right">
                                     <div class="project-item" style="background-image: url(/projecten/bekleding-wagonbelader.jpg);">
                                         <div class="project-item-overlay">
                                             <div class="project-item-title">Bekleding wagonbelader</div>
                                         </div>
                                     </div>
                                 </a>
-                                <a href="/projecten/fabrieksuitbreiding/" class="pull-right">
+                            </div>
+                            <div class="project-bar">
+                                <a href="/projecten/fabrieksuitbreiding/">
                                     <div class="project-item" style="background-image: url(/projecten/fabrieksuitbreiding.jpg);">
                                         <div class="project-item-overlay">
                                             <div class="project-item-title">Fabrieksuitbreiding</div>
                                         </div>
                                     </div>
                                 </a>
-                            </div>
-                            <div class="project-bar">
-                                <a href="/projecten/vliegtuigromp/">
+                                <a href="/projecten/vliegtuigromp/" class="pull-right">
                                     <div class="project-item" style="background-image: url(/projecten/vliegtuigromp.jpg);">
                                         <div class="project-item-overlay">
                                             <div class="project-item-title">Vliegtuigromp en bordes</div>
                                         </div>
                                     </div>
                                 </a>
-                                <a href="/projecten/buitentrap/" class="pull-right">
+                            </div>
+                            <div class="project-bar">
+                                <a href="/projecten/buitentrap/">
                                     <div class="project-item" style="background-image: url(/projecten/buitentrap.jpg);">
                                         <div class="project-item-overlay">
                                             <div class="project-item-title">Buitentrap</div>
                                         </div>
                                     </div>
                                 </a>
-                            </div>
-                            <div class="project-bar">
-                                <a href="/projecten/toegangsluik/">
+                                <a href="/projecten/toegangsluik/" class="pull-right">
                                     <div class="project-item" style="background-image: url(/projecten/toegangsluik.jpg);">
                                         <div class="project-item-overlay">
                                             <div class="project-item-title">Toegangsluik met valrooster</div>
                                         </div>
                                     </div>
                                 </a>
-                                <a href="/projecten/montage-en-transport-frame/" class="pull-right">
+                            </div>
+                            <div class="project-bar">
+                                <a href="/projecten/montage-en-transport-frame/">
                                     <div class="project-item" style="background-image: url(/projecten/montage-en-transport-frame.jpg);">
                                         <div class="project-item-overlay">
                                             <div class="project-item-title">Montage en transport frame</div>
---
+++ voscon.nl/projecten/kadekraan/index.php
@@ -0,0 +1,23 @@
+<?php require("../../includes/header.php"); ?>
+<?php require("../../includes/menu.php"); ?>
+			<div class="container">
+				<div id="sub-menu-bar">
+				</div>
+				<div id="items">
+                    <div class="item">
+                        <div class="header">
+                            <h2>Kadekraan</h2>
+                        </div>
+                        <div class="content columns-1">
+                            <div class="image-list">
+                                <img src="/projecten/kadekraan/IMG-20150406-WA0003.jpg" />
+                                <img src="/projecten/kadekraan/P4293784.jpg" />
+                                <img src="/projecten/kadekraan/P4293785.jpg" />
+                                <img src="/projecten/kadekraan/foto 1.jpg" />
+                                <img src="/projecten/kadekraan/foto 2.jpg" />
+                            </div>
+                        </div>
+                    </div>
+				</div>
+			</div>
+<?php require("../../includes/footer.php"); ?>
